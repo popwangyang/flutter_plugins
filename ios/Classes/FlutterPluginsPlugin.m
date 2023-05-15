4b1da8aca683661eebc9b815a63bc56542cfed8d
wangyan <1375441740@qq.com>
2023-05-15 18:14:36 +0800

    tij

--- ios/Classes/FlutterPluginsPlugin.m
+++ ios/Classes/FlutterPluginsPlugin.m
@@ -1,15 +1,93 @@
 #import "FlutterPluginsPlugin.h"
-#if __has_include(<flutter_plugins/flutter_plugins-Swift.h>)
-#import <flutter_plugins/flutter_plugins-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_plugins-Swift.h"
-#endif
+#import <AlicloudTLog/AlicloudTlogProvider.h>
+#import <AlicloudHAUtil/AlicloudHAProvider.h>
+#import <TRemoteDebugger/TRDManagerService.h>
+
+#import <TRemoteDebugger/TLogBiz.h>
+#import <TRemoteDebugger/TLogFactory.h>
+#import <UTDID/UTDevice.h>
 
 @implementation FlutterPluginsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftFlutterPluginsPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_plugins"
+            binaryMessenger:[registrar messenger]];
+  FlutterPluginsPlugin* instance = [[FlutterPluginsPlugin alloc] init];
+  NSLog(@"\n ==== %@",[UTDevice utdid]); // YZ2gRyP4t0EDAK4z2x7IRG+C
+      NSLog(@"这是插件oc代码");
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    NSString *message = call.arguments[@"message"];
+    NSString *module = call.arguments[@"module"];
+    TLogBiz *log = [TLogFactory createTLogForModuleName:module];
+    NSLog(@"\n ==== %@",[UTDevice utdid]);
+    NSLog(call.method);
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+
+  }else if([@"debug" isEqualToString:call.method]){
+     NSLog(message);
+    [log debug:message];
+
+  }else if([@"error" isEqualToString:call.method]){
+    [log error:message];
+    NSLog(message);
+  }else if([@"warn" isEqualToString:call.method]){
+    [log warn:message];
+    NSLog(message);
+  }else if([@"info" isEqualToString:call.method]){
+    [log info:message];
+    NSLog(message);
+  }else if([@"comment" isEqualToString:call.method]){ // 主动上传日志
+    [AlicloudTlogProvider uploadTLog:@"COMMENT"];
+
+  }else if([@"init" isEqualToString:call.method]){ // alicloud
+
+     /*
+         appKey
+         appSecret
+         tlogRsaSecret
+         appVersion // App版本
+         channel // 渠道标记 固定外界可不传
+         nick  // 用户昵称  都不能为空
+     */
+     NSString *appKey = call.arguments[@"appKeyIos"];
+     NSString *appSecret = call.arguments[@"appSecretIos"];
+     NSString *tlogRsaSecret = call.arguments[@"rsaPublicKeyIos"];
+     NSString *type = call.arguments[@"type"];
+     NSLog(@"params:");
+     NSLog(appKey);
+     NSLog(appSecret);
+     NSLog(tlogRsaSecret);
+     NSLog(type);
+     NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+     NSString *appVersion = [infoDictionary objectForKey:@"CFBundleShortVersionString"];; //配置项：App版本号
+     NSString *channel = call.arguments[@"channel"]; //配置项：渠道标记
+     NSString *nick = call.arguments[@"userNick"]; //配置项：用户昵称
+     NSLog(appVersion);
+     NSLog(channel);
+     NSLog(nick);
+     //配置项：控制台可拉取的日志级别
+     [[AlicloudTlogProvider alloc] initWithAppKey:appKey secret:appSecret tlogRsaSecret:tlogRsaSecret appVersion:appVersion channel:channel nick:nick];
+     [AlicloudHAProvider start];
+     // 拉取日志级别
+     NSInteger logLevel;
+     if([type isEqualToString:@"i"]){
+         logLevel = TLogLevelInfo;
+     }else if([type isEqualToString:@"w"]){
+         logLevel = TLogFlagWarn;
+     }else if([type isEqualToString:@"e"]){
+         logLevel = TLogFlagError;
+     }else{
+         logLevel = TLogLevelDebug; // d
+     }
+     [TRDManagerService updateLogLevel:logLevel];
+
+  }else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
--- ios/flutter_plugins.podspec
+++ ios/flutter_plugins.podspec
@@ -6,20 +6,20 @@ Pod::Spec.new do |s|
   s.name             = 'flutter_plugins'
   s.version          = '0.0.1'
   s.summary          = 'A new flutter plugin project.'
+  s.static_framework = true #静态库
   s.description      = <<-DESC
 A new flutter plugin project.
                        DESC
-  s.homepage         = 'http://example.com'
-  s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
-  s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.dependency 'Flutter'
-  s.platform = :ios, '9.0'
+s.homepage         = 'http://example.com'
+s.license          = { :file => '../LICENSE' }
+s.author           = { 'Your Company' => 'email@example.com' }
+s.source           = { :path => '.' }
+s.source_files = 'Classes/**/*'
+s.public_header_files = 'Classes/**/*.h'
+s.dependency 'Flutter'
+s.dependency 'AlicloudTLog' # aliyun
+s.platform = :ios, '9.0'
   
-  s.ios.vendored_frameworks = 'Frameworks/OneSDK/*.framework'
-  s.vendored_frameworks = 'AlicloudTLog.framework'
-
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
